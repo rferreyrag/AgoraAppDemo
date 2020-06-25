b40f8d1b1e82ea49f21b28fc7ddbc79ba1bb0dad
Raúl Ferreyra <60895320+rferreyrag@users.noreply.github.com>
2020-06-25 15:33:20 -0500

    primer  commit con eclipse. Close #1.

---
+++ AgoraAplicationDemo/src/main/java/com/agora/AgoraAplicationDemoApplication.java
@@ -0,0 +1,13 @@
+package com.agora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgoraAplicationDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgoraAplicationDemoApplication.class, args);
+	}
+
+}

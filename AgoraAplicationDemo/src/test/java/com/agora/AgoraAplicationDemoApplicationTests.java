35058de592df76e6efa8e6a9bda385f8725acfbf
Raúl Ferreyra <60895320+rferreyrag@users.noreply.github.com>
2020-06-25 15:33:20 -0500

    primer  commit con eclipse. Finish #1.

---
+++ AgoraAplicationDemo/src/test/java/com/agora/AgoraAplicationDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgoraAplicationDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
